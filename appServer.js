028be716d8b7dae9094af0b11f1b439cd38939ee
AdTekDev <nguyenngtu777@gmail.com>
2022-09-19 11:54:31 +0700

    Update appServer.js

--- appServer.js
+++ appServer.js
@@ -6,8 +6,7 @@ const fs = require("fs");
 
 // ------------------- Routing
 router.get( "/" , (yeucau, trave) => {
-    data = fs.readFileSync("./html/main.html");
-    pageContent = data.toString();
+    pageContent = "Hello, chua co gi !!!";
     trave.send(pageContent);
 });
 
